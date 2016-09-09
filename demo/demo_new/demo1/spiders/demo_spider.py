f41f47ed714312711701da8fb15e68711949e61b
wtq <1906272972@qq.com>
2016-09-09 15:08:48 +0800

    refactor

--- demo/demo_new/demo1/spiders/demo_spider.py
+++ demo/demo_new/demo1/spiders/demo_spider.py
@@ -5,10 +5,7 @@ __author__ = 'wtq'
 import os, sys
 import scrapy
 from scrapy.selector import Selector
-
-parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, parentdir)
-
+from demo.demo_new.demo1.items import DemoItem
 
 
 class DemoSpider(scrapy.Spider):
@@ -23,4 +20,15 @@ class DemoSpider(scrapy.Spider):
         # filename = response.url.split("/")[-2]
         # with open(filename, 'wb') as f:
         #     f.write(response.body)
-        sel = Selector()
+        sel = Selector(response)
+        sites = sel.xpath('//ul[@class="directory-url"]/li')
+        items = []
+
+        for site in sites:
+            item = DemoItem()
+            item['name'] = site.xpath('a/text()').extract()
+            item['url'] = site.xpath('a/@href').extract()
+            item['description'] = site.xpath('text()').re('-\s[^\n]*\\r')
+            items.append(item)
+
+        return items
